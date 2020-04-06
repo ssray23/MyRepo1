56ef3b98a6efebdfabfa46fcd043dfe6f37aa2a1
Suddha Satta Ray <ray.suddhasatta@gmail.com>
2020-04-06 16:42:20 +0100

    Added Java Source Files

---
+++ HumanClass.java
@@ -0,0 +1,10 @@
+public class HumanClass {
+
+    private static int i = 10;
+
+    public static void main(String args[]){
+        i = i + 1;
+        System.out.println("i is " + i);
+    }
+
+}
---
+++ PrintSomething.java
@@ -0,0 +1,10 @@
+package com.suddha;
+
+public class PrintSomething {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello Java ...");
+    }
+}
