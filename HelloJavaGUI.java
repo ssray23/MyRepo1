4feb794c174fb88c68bb8dd763c4ca75267fa6c9
ssray23 <ray.suddhasatta@gmail.com>
2020-04-07 11:12:31 +0100

    Commit

---
+++ HelloJavaGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class HelloJavaGUI {
+
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame( "Hello, Java!" );
+        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+        frame.add(label);
+        frame.setSize( 300, 300 );
+        frame.setVisible( true );
+    }
+}
