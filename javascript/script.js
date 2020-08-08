481cddc1c8abe6fc5ceb14afab18d395369b3a88
Suddha Satta Ray <ray.suddhasatta@gmail.com>
2020-08-08 14:49:15 +0100

    Javascript examples

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+let myConstant = 1;
+
+function addNumbers(num1,num2){
+	return num1+num2+myConstant;
+}
+let a = addNumbers(5,7);
+showMessage(a);
+console.log("Suddha Satta Ray");
---
+++ javascript/utils.js
@@ -0,0 +1,4 @@
+function showMessage(message) {
+	// method names such as getElementById() are case sensitive...
+    document.getElementById('demo').textContent = message;
+}
