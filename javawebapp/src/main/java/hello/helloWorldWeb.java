df5e47e75c8be88954bf0b9dbd77e7c7df62e041
Satta Ray <suddha.satta.ray@accenture.com>
2020-10-10 09:05:39 +0530

    Java Web App Hello World using tomcat

---
+++ javawebapp/src/main/java/hello/helloWorldWeb.java
@@ -0,0 +1,28 @@
+package hello;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class helloWorldWeb extends HttpServlet{
+	private String message;
+
+	public void init() throws ServletException{
+		message = "Hello World Web";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException{
+
+      	// Set response content type
+      	response.setContentType("text/html");
+
+      	// Actual logic goes here.
+      	PrintWriter out = response.getWriter();
+      	out.println("<h1>" + message + "</h1>");
+      }
+
+  	public void destroy() {
+      // do nothing.
+   }
+
+}
